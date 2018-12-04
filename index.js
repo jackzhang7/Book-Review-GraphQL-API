c4a99bff0c0f79efe8f2219ec354e7bcad625d50
Jiongbo Zhang <bob2007zhang@gmail.com>
2018-12-03 23:30:35 -0800

    Added cors and fixed mock data

--- index.js
+++ index.js
@@ -1,59 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
 
 // Some fake data
 const books = [
   {
+    id: '1',
     title: "Harry Potter and the Sorcerer's stone",
     author: 'J.K. Rowling',
     genre: 'Fantasy',
     rating: '4.46',
   },
   {
+    id: '2',
     title: 'Jurassic Park',
     author: 'Michael Crichton',
     genre: 'Sci-Fi',
     rating: '3.98',
   },
   {
+    id: '3',
     title: 'Pride and Prejudice',
     author: 'Jane Austen',
     genre: 'Literary Fiction',
     rating: '4.25',
   },
   {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-    genre: 'Sci-Fi',
-    rating: '3.98',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-    genre: 'Sci-Fi',
-    rating: '3.98',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-    genre: 'Sci-Fi',
-    rating: '3.98',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-    genre: 'Sci-Fi',
-    rating: '3.98',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-    genre: 'Sci-Fi',
-    rating: '3.98',
-  },
-  {
+    id: '4',
     title: 'Jurassic Park',
     author: 'Michael Crichton',
     genre: 'Sci-Fi',
@@ -64,7 +39,7 @@ const books = [
 // The GraphQL schema in string form
 const typeDefs = `
   type Query { books: [Book] }
-  type Book { title: String, author: String, genre: String, rating: String }
+  type Book { id: String, title: String, author: String, genre: String, rating: String }
 `;
 
 // The resolvers
@@ -80,6 +55,8 @@ const schema = makeExecutableSchema({
 
 // Initialize the app
 const app = express();
+
+app.use(cors())
 
 // The GraphQL endpoint
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
